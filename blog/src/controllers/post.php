abe806e1b582e44573de2aef35c455ec76649c5f
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-20 20:14:49 +0200

    Préparation pour la POO

--- blog/src/controllers/post.php
+++ blog/src/controllers/post.php
@@ -1,13 +1,12 @@
 <?php
-// src/controllers/post.php
 
 require_once('src/model.php');
 require_once('src/model/comment.php');
 
 function post(string $identifier)
 {
-	$post = getPost($identifier);
-	$comments = getComments($identifier);
+    $post = getPost($identifier);
+    $comments = getComments($identifier);
 
-	require('templates/post.php');
+    require('templates/post.php');
 }
